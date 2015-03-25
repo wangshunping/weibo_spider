4cd6b45afc748d85c0c7cd14afc7ea5eacc69cd1
wangshunping <lionelwang93@163.com>
2015-03-25 11:32:57 +0800

    modify util.py 2015.03.25
    modify util.py and now the file util.py can test if the weibo_spider can spider
    and the database if can connect success~

--- util.py
+++ util.py
@@ -1,9 +1,41 @@
+#-*- coding:utf-8 -*-
 import requests
 import gzip
 import StringIO
 import ConfigParser
 import sys
 from bs4 import BeautifulSoup
+import time
+import re
+import MySQLdb
+
+homePage = 'http://weibo.cn/'
+infoPage = '/info'
+
+def impRe(target):
+    pattern = u'昵称:([^\|]*)|'
+    abc = re.match(pattern, target)
+    name = abc.group(1)
+
+    pattern = u'性别:([^\|])'
+    abc = re.search(pattern, target)
+    sex = abc.group(1)
+
+    pattern = u'地区:([^\|]*)'
+    abc = re.search(pattern, target)
+    hometown = abc.group(1)
+
+    print name,sex,hometown
+    return name,sex,hometown
+
+def reGetUid(inputstr):
+    import re
+    patt = 'uid=([0-9]*)&rl'
+    qwe = re.search(patt,inputstr)
+    try:
+        return qwe.group(1)
+    except:
+        return None
 
 def get_content(toUrl):
     """ Return the content of given url
@@ -31,28 +63,68 @@ def get_content(toUrl):
         sys.exit(-1)
     return req
 
-def processUrl(content):
-    soup = BeautifulSoup(content)
-    print soup.prettify()
-    qwe = [link.get('href') for link in soup.find_all('a') if 'uid' in link.get('href')]
-    print qwe
+def getUniversityStudent(inputid,school):
+    time_now = int(time.time())
+    inputUrl = homePage + inputid + infoPage
+    tmpContent = get_content(inputUrl)
+    soup = BeautifulSoup(tmpContent.text)
+    time.sleep(1)
+    divlabel = soup.find_all('div','tip')
 
-def reGetUid(inputstr):
-    import re
-    patt = 'uid=([0-9]*)&rl'
-    qwe = re.search(patt,inputstr)
+    personalInfo = divlabel[0].next_sibling.get_text('|',strip=True)
+    schoolInfo = divlabel[1].next_sibling.get_text()
+
+    if school in schoolInfo:
+        name,sex,hometown = impRe(personalInfo)
+        return [(name,0,inputid,time_now,sex,hometown)]
+    else:
+        pass
+
+def connectToDatabase(firstInfo):
+    cf = ConfigParser.ConfigParser()
+    cf.read('config.ini')
+    host = cf.get("db","host")
+    port = int(cf.get("db","port"))
+    user = cf.get("db","user")
+    passwd = cf.get("db","passwd")
+    db_name = cf.get("db","db")
+    charset = cf.get("db","charset")
+    use_unicode = cf.get("db","use_unicode")
     try:
-        return qwe.group(1)
+        db = MySQLdb.connect(host=host, port=port, user=user, \
+                              passwd=passwd, db=db_name, \
+                              charset=charset,use_unicode=use_unicode)
+        cursor = db.cursor()
     except:
-        return None
+        print "Fail to connect to database"
+        sys.exit(-1)
+    print "connet to databse success ..."
+    sql = "INSERT IGNORE INTO NAME (USERNAME,LAST_VISIT,LINK_ID,ADD_TIME,SEX,HOMETOWN) VALUES(%s,%s,%s,%s,%s,%s)"
+    cursor.execute(sql, firstInfo)
+    print "insert first info to database success ..."
 
 def main():
-    url='http://weibo.cn/1738181262/follow?page=9'
+    print "weibo spider test begin ~~"
+    print "----------"
+    cf = ConfigParser.ConfigParser()
+    cf.read('config.ini')
+    school = cf.get("weibo","school").decode('utf-8')
+    mainid = cf.get("weibo","mainID")
+
+    url = homePage + mainid
     content = get_content(url)
-    processUrl(content.text)
-    #namestr = 'http://weibo.cn/attention/add? \
-               #uid=3889609082&rl=1&st=d159be'
-    #reGetUid(namestr)
+    firstInfo = getUniversityStudent(mainid, school)
+    if firstInfo != None:
+        print "weibo spider test pass"
+    else:
+        print "weibo spider test fail... "
+        print "Cause func getUniveristyStudent return None "
+        sys.exit(-2)
+    print "----------"
+    connectToDatabase(firstInfo)
+
+
+
 
 if __name__ == "__main__":
     main()
